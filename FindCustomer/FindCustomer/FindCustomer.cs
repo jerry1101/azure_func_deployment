b7489964da23f4e0ba383e9227e87ee55da7aaf3
jerry <jerry110160@gmail.com>
2018-12-15 19:41:16 -0500

    Find by id

--- FindCustomer/FindCustomer/FindCustomer.cs
+++ FindCustomer/FindCustomer/FindCustomer.cs
@@ -6,6 +6,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using FuncDLL;
+using System;
 
 namespace FindCustomer
 {
@@ -16,14 +18,16 @@ namespace FindCustomer
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string id = req.Query["id"];
 
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            id = id ?? data?.id;
+            var customer = new CustomerDA().GetCustomerByID(Convert.ToInt32(id));
+            var jsonToReturn = JsonConvert.SerializeObject(customer);
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Morning, {name}")
+            return customer != null
+                ? (ActionResult)new OkObjectResult($"Morning, {jsonToReturn}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
